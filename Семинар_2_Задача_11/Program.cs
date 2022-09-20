dd3c0f1693c554d65710029bd63d62fa5d5faec6
Alexandr Fetisov <fav13233@mail.ru>
2022-09-20 16:48:11 +0300

    Семинар 2

---
+++ Семинар_2_Задача_11/Program.cs
@@ -0,0 +1,16 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+
+int random = new Random().Next(100, 1000);
+Console.WriteLine($"Число: {random}");
+
+int numberA = random / 100;
+int numberB = random % 10;
+
+Console.Write(numberA);
+Console.Write(numberB);
+
---
+++ Семинар_2_Задача_13_ДЗ/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberstr = Convert.ToString(number);
+if (numberstr.Length > 2)
+{
+  Console.WriteLine("третья цифра  " + numberstr[2]);
+}
+else if (numberstr.Length < 2)
+{
+  Console.WriteLine("-> второй цифры нет");
+}
+else 
+{
+  Console.WriteLine("-> третьей цифры нет");
+}
